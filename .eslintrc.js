1c3390ed9e02371432a2743fbbe5f589e36dc853
Zsolt Hajdu <z_hajdu@yahoo.com>
2022-11-05 11:58:34 -0700

    Fixed process for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     "es6": true,
     "jest/globals": true
   },
+  "globals": {
+    "process": true
+  },
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
